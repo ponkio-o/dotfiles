ba4465fda8d1f1073748f277f555190d74c4a91b
ponkio-o <pasokon218@gmail.com>
2019-01-31 22:13:09 +0900

    Add create_link.sh

---
+++ create_link.sh
@@ -0,0 +1,7 @@
+ln -sf ~/github/dotfiles/shell/.alias ~/.alias
+ln -sf ~/github/dotfiles/shell/.zprofile ~/.zprofile
+ln -sf ~/github/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/github/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/github/dotfiles/vim/colors/molokai.vim ~/.vim/colors/molokai.vim
+
+
