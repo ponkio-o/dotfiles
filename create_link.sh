2e4395e8cededc5b9f735fb79024c04181f82f13
ponkio-o <pasokon218@gmail.com>
2020-03-04 17:31:00 +0900

    FIX: PATH を変更

--- create_link.sh
+++ create_link.sh
@@ -1,7 +1,7 @@
-ln -sf ~/github/dotfiles/shell/.alias ~/.alias
-ln -sf ~/github/dotfiles/shell/.zprofile ~/.zprofile
-ln -sf ~/github/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/github/dotfiles/vim/.vimrc ~/.vimrc
-ln -sf ~/github/dotfiles/vim/colors/molokai.vim ~/.vim/colors/molokai.vim
-
+ln -sf ~/git/dotfiles/shell/.alias ~/.alias
+ln -sf ~/git/dotfiles/shell/.zprofile ~/.zprofile
+ln -sf ~/git/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/git/dotfiles/vim/.vimrc ~/.vimrc
+ln -sf ~/git/dotfiles/vim/colors/molokai.vim ~/.vim/colors/molokai.vim
+ln -sf ~/git/dotfiles/shell/zsh-completions ~/.zsh-completions
 
