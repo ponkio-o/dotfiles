969104eb197f3bf1036153cb43763e53d10094fa
ponkio-o <29038315+ponkio-o@users.noreply.github.com>
2026-01-26 11:17:48 +0900

    remove pet

--- setup.sh
+++ setup.sh
@@ -77,11 +77,6 @@ sudo ln -sf $DIR/script/get_ssid_tmux /usr/local/bin/get_ssid_tmux
 sudo cp $DIR/bin/realpath /usr/local/bin/realpath
 sudo cp $DIR/bin/docker-credential-gh /usr/local/bin/docker-credential-gh
 
-# pet (snipet tool)
-mkdir -p $XDG_CONFIG_HOME/pet
-ln -sf $DIR/pet/config.toml $XDG_CONFIG_HOME/pet/config.toml
-ln -sf $DIR/pet/snippet.toml $XDG_CONFIG_HOME/pet/snippet.toml
-
 # myaws
 ln -sf $DIR/myaws/.myaws.yml $HOME/.myaws.yml
 
